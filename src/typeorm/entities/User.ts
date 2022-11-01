92f2ca621d4923acb1300e08f4383917765d2dcf
ezzat <ezzatmakar93@gmail.com>
2022-11-02 01:24:53 +0200

    project setup

---
+++ src/typeorm/entities/User.ts
@@ -0,0 +1,27 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ nullable: true })
+  authStrategy: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+}
